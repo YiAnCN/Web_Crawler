9a59e65a89d6cc99d36bb93ce676089fa2ea7fb2
Williams_Z <30449995+WilliamsZhang@users.noreply.github.com>
2017-09-23 14:42:28 +0800

    Create 豆瓣烂片.py

---
+++ 豆瓣烂片.py
@@ -0,0 +1,36 @@
+爬取豆瓣烂片top250
+
+import requests
+import time
+from bs4 import BeautifulSoup
+
+def get_page(url):
+    r=requests.get(url)
+    r.raise_for_status()
+    return r.text
+
+def parse_page(html):
+    soup=BeautifulSoup(html,'lxml')
+    divs=soup.find_all('div','pl2')
+    for div1 in divs:
+        try:
+            div2=div1.find('a').get_text().replace(' ','')
+            div3=div1.find('p').get_text()
+            div4=div1.find('div').find_all('span')[1].get_text()
+            div5 = div1.find('div').find_all('span')[2].get_text()
+            with open(r'C:\Users\Williams_Z\Desktop\movie5.txt','a+',encoding='utf-8') as f:
+                content=div2 + ';' + div3 + ';' + div4 +' '+'\\'+' '+ div5 +'\n'
+                f.write(content)
+        except:
+            continue
+    time.sleep(1)
+
+def main(page):
+    url='https://movie.douban.com/tag/%E7%83%82%E7%89%87?start={}'.format(page)
+    html=get_page(url)
+    parse_page(html)
+
+if __name__ == '__main__':
+    for page in range(106):
+        main(page*20)
+    print('Good Job')
