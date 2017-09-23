43ac58113884c4d79866f852db80272195e61a94
Williams_Z <30449995+WilliamsZhang@users.noreply.github.com>
2017-09-23 19:53:39 +0800

    Update selenium自动化测试.py

--- selenium自动化测试.py
+++ selenium自动化测试.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 driver=webdriver.Chrome()
 
@@ -7,4 +8,13 @@ driver.get(url)
 
 driver.find_element_by_xpath('  ').send_keys('input what you want to')   #向输入框输入字符
 driver.find_element_by_xpath('  ').click()                               #点击
+driver.find_element_by_xpath('  ').clear()                               #清除框内信息
 
+soup=Beautiful(driver.page_source,'lxml')                                #driver.page_source 即为获取网页源代码，此处即解析源代码
+
+iitems=soup.find_all('div','')                                           #接下来就是正常的用 BeautifulSoup 进行解码了
+for item in items:
+  ......
+
+
+# 之后详细去看一下官方文档之后再来补充（2017/9/23）
