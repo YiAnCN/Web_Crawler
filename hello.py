d53c3f8767ee3cea00f468e392a5967fed2d9f2e
Williams_Z <30449995+WilliamsZhang@users.noreply.github.com>
2017-10-22 01:26:16 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -17,20 +17,20 @@ def get_page(url):
 def parse_page(html):
     try:
         Data=json.loads(html)
-        # for item in Data['data']:
-        #     yield item['article_url']
-    # return Data.get('data')
-
-        if Data and 'data' in Data.keys():
+        if Data and 'data' in Data.keys():    #用于判断'data'是否在Data这个字典中，也可以写成 if 'data' in Data.keys():
+            ---------------------------------
             for item in Data.get('data'):
                 yield item.get('article_url')
+            --------------------------------
+            for item in Data['data']:
+                yield item['article_url']
+            -------------------------------
+            #两种写法均可以
     except:
         pass
     # htmldict=Data['data']
-    # yield htmldict.get('article_url')
-
-
-    # return htmldict
+    # for item in htmldict:
+        # yield item.get('article_url')
 
 def main():
     url='https://www.toutiao.com/search_content/'
